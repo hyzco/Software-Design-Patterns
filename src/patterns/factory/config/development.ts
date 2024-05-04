f3e7287228df7d60fb1a18be2afd6bc6a4999d95
hyzco <hsn.yzg@gmail.com>
2024-05-04 16:45:19 +0200

    remove unused files

--- src/patterns/factory/config/development.ts
+++
@@ -1,6 +0,0 @@
-import { ILogStorage, EStorageType } from "../ILogStorage";
-
-export default class DevelopmentLog implements ILogStorage {
-    storageType: EStorageType;
-    log: (log: string) => void;
-}
--- src/patterns/factory/config/production.ts
+++
@@ -1,6 +0,0 @@
-import { ILogStorage, EStorageType } from "../ILogStorage";
-
-export default class ProductionLog implements ILogStorage {
-    storageType: EStorageType;
-    log: (log: string) => void;
-}
